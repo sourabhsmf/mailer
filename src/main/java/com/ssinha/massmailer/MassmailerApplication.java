5b12b743c761ba3dae8a81cf51a71ef158b1dad1
sourabhsmf <sourabhs@mindfiresolutions.com>
2020-04-14 09:48:03 +0530

    Initial commit

---
+++ src/main/java/com/ssinha/massmailer/MassmailerApplication.java
@@ -0,0 +1,13 @@
+package com.ssinha.massmailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassmailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassmailerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssinha/massmailer/MassmailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssinha.massmailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassmailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
